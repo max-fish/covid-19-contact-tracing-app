48db5cada77373f06bbc3bfcaf8c24d69e828f01
max-fish <maxim.fishman@outlook.com>
2021-01-26 13:55:53 -0800

    add streaming shared preferences

--- lib/widgets/contactTracingAlertDialog.dart
+++ lib/widgets/contactTracingAlertDialog.dart
@@ -1,4 +1,4 @@
-import '../utilities/sharedPreferences.dart';
+import '../utilities/userPreferences.dart';
 import 'package:flutter/material.dart';
 
 Future<void> showContactTracingAlertDialog(BuildContext context) {
