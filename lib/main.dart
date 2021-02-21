35d9d2255d75bc8265c24f680aaf0ccb5738ebc7
max-fish <maxim.fishman@outlook.com>
2021-02-20 20:12:11 -0800

    move FutureBuilder to main file to optimize loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'models/covidMarkerModel.dart';
+import 'models/coronavirusDataModel.dart';
 import 'pages/interactions/interactions.dart';
 import 'pages/test_result/testResult.dart';
+import 'utilities/assetUtilities.dart';
+import 'utilities/coronavirusData.dart';
 import 'utilities/userPreferences.dart';
 import 'widgets/covidHotspotMap.dart';
 import 'widgets/dragSection.dart';
@@ -14,9 +18,9 @@ import 'pages/symptoms/symptomsSelection.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  if(FirebaseAuth.instance.currentUser == null) {
-    final UserCredential userCredential = await FirebaseAuth.instance
-        .signInAnonymously();
+  if (FirebaseAuth.instance.currentUser == null) {
+    final UserCredential userCredential =
+        await FirebaseAuth.instance.signInAnonymously();
     print(userCredential.user.uid);
   }
   ContactTracingUtilities.init();
@@ -46,6 +50,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  Future<List<CovidMarkerModel>> _getCovidData() async {
+    final List<CovidMarkerModel> covidMarkerData = List<CovidMarkerModel>();
+    final List<CoronavirusDataModel> upperTierCovidData =
+    await CoronavirusData.getUpperTierData();
+    final List<CoronavirusDataModel> lowerTierCovidData =
+    await CoronavirusData.getLowerTierData();
+    final nameToCoordinates = await AssetUtils.loadLocaAuthorityCoordinates();
+    final List<CoronavirusDataModel> allTierCovidData = [
+      ...upperTierCovidData,
+      ...lowerTierCovidData
+    ];
+    for (CoronavirusDataModel covidDataInArea in allTierCovidData) {
+      final String areaName = covidDataInArea.areaName;
+      final coordinates = nameToCoordinates[areaName];
+      if (coordinates != null && covidDataInArea.newCases != 0) {
+        covidMarkerData.add(CovidMarkerModel(
+            areaName: areaName,
+            newCases: covidDataInArea.newCases,
+            latitude: coordinates['lat'],
+            longitude: coordinates['long']));
+      }
+    }
+    return covidMarkerData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,84 +88,100 @@ class _MyHomePageState extends State<MyHomePage> {
       //         .copyWith(color: Colors.white),
       //   ),
       // ),
-      body: Stack(
-        children: [
-          CovidHotspotMap(),
-          Align(
-            alignment: const Alignment(0, -0.85),
-            child: Container(
-              height: 50,
-              width: MediaQuery.of(context).size.width - 10,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.all(Radius.elliptical(50, 50)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3)
-                  )
-                ]
-              ),
-              child: const Align(
-                alignment: Alignment(-0.85, 0),
-                  child: Text('Go somewhere', style: TextStyle(color: Colors.blueGrey),)
-              ),
-            ),
-          ),
-          SnappingSheet(
-            sheetBelow: SnappingSheetContent(
-              child: Container(
-                color: Colors.white,
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  children: [
-                    PageButton(
-                      icon: Icons.thermostat_rounded,
-                      pageName: 'Check Symptoms',
-                      onPress: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => SymptomsSelection())
-                        );
-                      },
+      body: FutureBuilder<List<CovidMarkerModel>>(
+        future: _getCovidData(),
+        builder: (BuildContext context, AsyncSnapshot<List<CovidMarkerModel>> snapshot) {
+          if(snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+              children: [
+                CovidHotspotMap(covidData: snapshot.data,),
+                Align(
+                  alignment: const Alignment(0, -0.85),
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width - 10,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                        const BorderRadius.all(Radius.elliptical(50, 50)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: const Offset(0, 3))
+                        ]),
+                    child: const Align(
+                        alignment: Alignment(-0.85, 0),
+                        child: Text(
+                          'Go somewhere',
+                          style: TextStyle(color: Colors.blueGrey),
+                        )),
+                  ),
+                ),
+                SnappingSheet(
+                  sheetBelow: SnappingSheetContent(
+                    child: Container(
+                      color: Colors.white,
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        children: [
+                          PageButton(
+                            icon: Icons.thermostat_rounded,
+                            pageName: 'Check Symptoms',
+                            onPress: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          SymptomsSelection()));
+                            },
+                          ),
+                          PageButton(
+                              icon: Icons.input_rounded,
+                              pageName: 'I am positive for COVID-19',
+                              onPress: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => TestResult()));
+                              }),
+                          PageButton(
+                            icon: Icons.list_alt_rounded,
+                            pageName: 'View Interactions',
+                            onPress: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Interactions()));
+                            },
+                          )
+                        ],
+                      ),
                     ),
-                    PageButton(
-                      icon: Icons.input_rounded,
-                      pageName: 'I am positive for COVID-19',
-                      onPress: () {
-                        Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => TestResult()));
-                      }
-                    ),
-                    PageButton(
-                      icon: Icons.list_alt_rounded,
-                      pageName: 'View Interactions',
-                      onPress: () {
-                        Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Interactions()));
-                      },
-                    )
+                    heightBehavior: const SnappingSheetHeight.fixed(),
+                  ),
+                  grabbing: DragSection(),
+                  grabbingHeight: 100,
+                  initSnapPosition: const SnapPosition(positionFactor: 0),
+                  snapPositions: [
+                    const SnapPosition(
+                        positionFactor: 0,
+                        snappingDuration: Duration(milliseconds: 100)),
+                    const SnapPosition(
+                        positionFactor: 0.5,
+                        snappingDuration: Duration(milliseconds: 100)),
                   ],
                 ),
-              ),
-              heightBehavior: const SnappingSheetHeight.fixed(),
-            ),
-            grabbing: DragSection(),
-            grabbingHeight: 100,
-            initSnapPosition: const SnapPosition(positionFactor: 0),
-            snapPositions: [
-              const SnapPosition(
-                positionFactor: 0,
-                snappingDuration: Duration(milliseconds: 100)
-              ),
-              const SnapPosition(
-                  positionFactor: 0.5,
-                  snappingDuration: Duration(milliseconds: 100)
-              ),
-            ],
-          ),
-        ],
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
--- lib/widgets/covidHotspotMap.dart
+++ lib/widgets/covidHotspotMap.dart
@@ -2,25 +2,21 @@ import 'dart:async';
 import '../models/covidMarkerModel.dart';
 import 'areaDescriptionTiles.dart';
 import '../utilities/covidHotspotIconGenerator.dart';
-import '../utilities/assetUtilities.dart';
-import '../models/coronavirusDataModel.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-import '../utilities/coronavirusData.dart';
 
 class CovidHotspotMap extends StatefulWidget {
   final LocationData location;
+  final List<CovidMarkerModel> covidData;
 
-  CovidHotspotMap({this.location});
+  CovidHotspotMap({this.location, @required this.covidData});
 
   @override
   _CovidHotspotMapState createState() => _CovidHotspotMapState();
 }
 
 class _CovidHotspotMapState extends State<CovidHotspotMap> {
-  final List<CovidMarkerModel> covidMarkerData = List<CovidMarkerModel>();
-
   Widget areaDescriptionTiles;
 
   Future<Map<String, dynamic>> coordinates;
@@ -38,37 +34,14 @@ class _CovidHotspotMapState extends State<CovidHotspotMap> {
     zoom: 11,
   );
 
-  Future<void> _getCovidData() async {
-    final List<CoronavirusDataModel> upperTierCovidData =
-        await CoronavirusData.getUpperTierData();
-    final List<CoronavirusDataModel> lowerTierCovidData =
-        await CoronavirusData.getLowerTierData();
-    final nameToCoordinates = await AssetUtils.loadLocaAuthorityCoordinates();
-    final List<CoronavirusDataModel> allTierCovidData = [
-      ...upperTierCovidData,
-      ...lowerTierCovidData
-    ];
-    for (CoronavirusDataModel covidDataInArea in allTierCovidData) {
-      final String areaName = covidDataInArea.areaName;
-      final coordinates = nameToCoordinates[areaName];
-      if (coordinates != null && covidDataInArea.newCases != 0) {
-        covidMarkerData.add(CovidMarkerModel(
-            areaName: areaName,
-            newCases: covidDataInArea.newCases,
-            latitude: coordinates['lat'],
-            longitude: coordinates['long']));
-      }
-    }
-  }
-
   Future<void> _setMarkers(BuildContext buildContext) async {
-    for (CovidMarkerModel covidMarkerModel in covidMarkerData) {
+    for (CovidMarkerModel covidMarkerModel in widget.covidData) {
       final _markerIdValue = 'marker_id_$_markerIdCounter';
       _markerIdCounter++;
       final BitmapDescriptor hotspotIcon =
           await generateCovidHotspotIcon(covidMarkerModel.newCases * 2.0);
       final PageController pageController = PageController(
-          initialPage: covidMarkerData.indexOf(covidMarkerModel));
+          initialPage: widget.covidData.indexOf(covidMarkerModel));
       _markers.add(Marker(
           markerId: MarkerId(_markerIdValue),
           icon: hotspotIcon,
@@ -79,7 +52,7 @@ class _CovidHotspotMapState extends State<CovidHotspotMap> {
                 context: buildContext,
                 builder: (BuildContext context) {
                   return AreaDescriptionTiles(
-                      covidData: covidMarkerData,
+                      covidData: widget.covidData,
                       pageController: pageController,
                       googleMapController: googleMapController);
                 });
@@ -89,37 +62,24 @@ class _CovidHotspotMapState extends State<CovidHotspotMap> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _getCovidData(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return GoogleMap(
-                mapType: MapType.normal,
-                initialCameraPosition: _kGooglePlex,
-                onMapCreated: (GoogleMapController controller) async {
-                  googleMapController = controller;
-                  if (!_controller.isCompleted) {
-                    showDialog(
-                        context: context,
-                        barrierDismissible: false,
-                        builder: (BuildContext context) {
-                          return Container(
-                            decoration:
-                                const BoxDecoration(color: Colors.white),
-                            child: const Center(
-                                child: CircularProgressIndicator()));
-                        }
-                    );
-                    await _setMarkers(context);
-                    Navigator.pop(context);
-                    setState(() {});
-                    _controller.complete(controller);
-                  }
-                },
-                markers: _markers);
-          } else {
-            return const Center(child: CircularProgressIndicator());
+    return GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) async {
+          googleMapController = controller;
+          if (!_controller.isCompleted) {
+            showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  return const Center(child: CircularProgressIndicator());
+                });
+            await _setMarkers(context);
+            Navigator.pop(context);
+            setState(() {});
+            _controller.complete(controller);
           }
-        });
+        },
+        markers: _markers);
   }
 }
