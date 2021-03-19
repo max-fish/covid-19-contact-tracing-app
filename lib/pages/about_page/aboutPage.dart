5b4b1735c6da8e81987e62d82ac6a465605829fe
max-fish <maxim.fishman@outlook.com>
2021-03-19 22:58:49 +0000

    fix about page grammar

--- lib/pages/about_page/aboutPage.dart
+++ lib/pages/about_page/aboutPage.dart
@@ -11,15 +11,15 @@ class AboutPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text('Welcome to the Walkdown contact tracing app. This app is designed to provide meaningful, real-time information about your surroundings regarding COVID-19 exposure. It uses a combination of bluetooth and sound signals to interact with nearby devices.'),
+              const Text('Welcome to the Walkdown contact-tracing app. This app is designed to provide meaningful, real-time information about your surroundings regarding COVID-19 exposure. It uses a combination of bluetooth and sound signals to interact with nearby devices.'),
               const SizedBox(height: 20,),
-              const Text('Person data is never shared to anybody else, because no personal data is ever collected Your user id is anonymized, so nobody knows who you are. The only data that we share to othe devices is your health status. For example, healthy, have symptoms, or positive test. This information is used to inform other users if your sick, to keep everybody safe.'),
+              const Text('Personal data is never shared to anybody else, because no personal data is ever collected from you. Your user id is anonymized, so nobody knows who you are. The only data that we share to other devices is your health status, and a notification token. This information is used to inform other users if you are sick, to keep everybody safe.'),
               const SizedBox(height: 20,),
               const Text('When searching for a place on a map, your location will be accessed in order to create a route. You location is only used for that instant, and it is never used or queried again.'),
               const SizedBox(height: 20,),
-              const Text('Thank you for using this app.'),
+              const Text('Thank you for using this app, and stay safe.'),
               const SizedBox(height: 20,),
-              const Text('Sincerely'),
+              const Text('Sincerely,'),
               const SizedBox(height: 10,),
               const Text('The Walkdown Team')
             ],
