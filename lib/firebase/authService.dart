7814ca1e1e1209d0ccde819e99da3d7efb681185
max-fish <maxim.fishman@outlook.com>
2021-03-21 14:04:02 +0000

    move all functionality to one authService.dart file

--- lib/firebase/authService.dart
+++ lib/firebase/authService.dart
@@ -1,9 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'firestoreService.dart';
+
 class AuthService {
   static final FirebaseAuth _auth = FirebaseAuth.instance;
 
   static User get user => _auth.currentUser;
+
+  static String get userId => _auth.currentUser.uid;
+
+  static Future<void> signInAnonIfNew() async {
+    if (FirebaseAuth.instance.currentUser == null) {
+      final UserCredential userCredential =
+      await FirebaseAuth.instance.signInAnonymously();
+      FirestoreService.addUser(userCredential.user.uid);
+    }
+  }
 
   static Future<void> signInAnon() async {
     if (FirebaseAuth.instance.currentUser == null) {
